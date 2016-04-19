eefaa32c682a67d42740d399951cc76bf5a53c13
AlenPelin <AlenPelin@gmail.com>
2016-04-20 00:19:04 +1000

    Release 1.2.3.0

--- src/Sitecore.JsonDataProvider/Properties/AssemblyInfo.cs
+++ src/Sitecore.JsonDataProvider/Properties/AssemblyInfo.cs
@@ -11,6 +11,6 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("Sitecore® is a registered trademark of Sitecore Corporation")]
 [assembly: ComVisible(false)]
 
-[assembly: AssemblyFileVersion("1.2.2.0")]
-[assembly: AssemblyInformationalVersion("1.2.2.0")]
-[assembly: AssemblyVersion("1.2.2.0")]
+[assembly: AssemblyFileVersion("1.2.3.0")]
+[assembly: AssemblyInformationalVersion("1.2.3.0")]
+[assembly: AssemblyVersion("1.2.3.0")]
